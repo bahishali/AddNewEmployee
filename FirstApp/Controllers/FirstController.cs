d233eb17cc360f505ab06b1a0aa34635c9ec75b8
USER <USER@USER-BILGISAYAR>
2023-03-09 14:33:29 +0300

    Add project files.

---
+++ FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Create(employee);
+                return View("Thanks", employee);
+            }
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Age = employee.Age;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Salary = employee.Salary;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Department = employee.Department;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Sex = employee.Sex;
+                Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
